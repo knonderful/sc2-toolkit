931ad195a6a89e0ed58d69a0ef853b9117952c2b
knonderful <knonderful@github.com>
2017-07-17 10:51:15 +0200

    Removed unused import.

--- apps/toolkit-app/src/main/java/sc2toolkit/app/toolkit/overwolf/HttpClient.java
+++ apps/toolkit-app/src/main/java/sc2toolkit/app/toolkit/overwolf/HttpClient.java
@@ -6,7 +6,6 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
-import io.netty.channel.EventLoopGroup;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
